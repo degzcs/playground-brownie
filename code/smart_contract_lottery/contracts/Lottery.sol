3958096eab6c154c22e82c1df942a5094d4a417b
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2022-05-18 15:09:07 -0500

    add lotter smart contract code

--- code/smart_contract_lottery/contracts/Lottery.sol
+++ code/smart_contract_lottery/contracts/Lottery.sol
@@ -2,14 +2,33 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract Lottery {
-
+contract Lottery is VRFConsumerBase, Ownable {
 	address payable[] public players;
+	address payable public recentWinner;
 	uint256 public usdEntryFee;
 	AggregatorV3Interface internal ethUsdPriceFeed;
+	enum LOTTERY_STATE {
+		OPEN,
+		CLOSED,
+		CALCULATING_WINNER
+	}
+	LOTTERY_STATE public lottery_state;
+	uint256 public fee;
+	bytes32 public keyHash;
+	uint256 public randomness;
 
-	constructor(address _priceFeedAddress) public {
+	constructor(
+		address _priceFeedAddress,
+		address _vrfCoordinator,
+		address _link,
+		uint256 _fee,
+		bytes32 _keyHash
+	)
+	public
+	VRFConsumerBase(_vrfCoordinator, _link){
 		// NOTE: this is the minimum value for each user
 		// to enter the lottery
 		// NOTE: the values should be in Wei always (no decimals in Solidity)
@@ -17,14 +36,20 @@ contract Lottery {
 
 		// Get the Price Feed from the XXX network
 		ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+		lottery_state = LOTTERY_STATE.CLOSED;
+
+		// This fee is the price that we have to pay for using
+		// the randomness oracle service
+		fee = _fee;
+
+		// This is the way to identify the uniqueness of the chainlink node
+		keyHash = _keyHash;
 	}
 
 	function enter() public payable {
+		require(lottery_state == LOTTERY_STATE.OPEN);
+		require(msg.value >= getEntranceFee(), "Not enough ETH!");
 		players.push(msg.sender);
-	}
-
-	function getVersion() public view returns (uint256){
-		return ethUsdPriceFeed.version();
 	}
 
 	function getEntranceFee() public view returns (uint256) {
@@ -37,20 +62,37 @@ contract Lottery {
 		// -> 2_025_77000000 -> 2_025_770000000000000000 (wei)
 		uint256 adjustedPrice = uint256(price) * 10**10;
 		// NOTE we are going to add 18 zeros to the entry dollars fee ($50)
-		// the idea is that those new 18 zeros are going to cancel out 
+		// the idea is that those new 18 zeros are going to cancel out
 		// with the zeros from price
 		// ex. 50_000000000000000000 (this is already wei)
-		// -> 50_000000000000000000_000000000000000000/2_025_770000000000000000 
+		// -> 50_000000000000000000_000000000000000000/2_025_770000000000000000
 		// 50_00000000000000000_00/2_025_77 -> 24547816664031531 (WEI)
 		uint256 costToEnter = (usdEntryFee * 10**18)/adjustedPrice;
 		return costToEnter;
 	}
 
-	function startLottery() public {
-
+	function startLottery() public onlyOwner {
+		require(lottery_state == LOTTERY_STATE.CLOSED,
+						"Can't start a new lottery yet!");
+		lottery_state = LOTTERY_STATE.OPEN;
 	}
 
-	function endLottery() public {
+	function endLottery() public onlyOwner {
+		lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+		// NOTE this function will start the request-response process
+		bytes32 requestId = requestRandomness(keyHash, fee);
+		// node is going to return the response to a fulfillRandomness function
+	}
 
+	function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override {
+		require(lottery_state == LOTTERY_STATE.CALCULATING_WINNER, "You aren't there yet!");
+		require(_randomness > 0, "randm-not-found");
+		uint256 indexOfWinner = _randomness % players.length;
+		recentWinner = players[indexOfWinner];
+		recentWinner.transfer(address(this).balance);
+		// Reset
+		players = new address payable[](0);
+		lottery_state = LOTTERY_STATE.CLOSED;
+		randomness = _randomness;
 	}
 }
